bd1a2bfd885fe9d0f30706600ac0dde3a37eeaee
codybenlewis <cblewisnj@gmail.com>
2020-04-15 02:49:03 -0400

    Added Spotify bar to top of page

--- assets/js/spotify.js
+++ assets/js/spotify.js
@@ -1,12 +1,27 @@
-// $.ajax({
-//      url: "http://192.168.0.170/",
-//      success: function (data) {
-//        console.log(data)
-//        alert(data);
-//      }
-//    });
-//
+function getSpotify() {
+  $.getJSON('https://api.codybenlewis.com/spotify/current', function(data) {
+    if (data.current != null) {
+      // we have data
+      console.log(data);
+      var live = '<img id="live" src="/assets/images/live.png">';
+      var intro = "I'm currently listening to ";
+      var track = '<a href="' + data.current.track_link + '">' + data.current.track + '</a> ';
+      var artist = 'by <a href="' + data.current.artist_link + '">' + data.current.artist;
+      var outro = '</a> on Spotify. Like, right now.';
+      $(spotify).html(live + intro + track + artist + outro);
+    } else {
+      $.getJSON('https://api.codybenlewis.com/spotify/recent', function(data) {
+        console.log(data);
+        var intro = 'I was just listening to ';
+        var track = '<a href="' + data.recent[0].track_link + '">' + data.recent[0].track + '</a> ';
+        var artist = 'by <a href="' + data.recent[0].artist_link + '">' + data.recent[0].artist;
+        var outro = "</a> on Spotify, but nothing's playing now.";
+        $(spotify).html(intro + track + artist + outro);
+      });
+    }
+  });
+}
 
-$.getJSON('https://codybenlewis.duckdns.org/spotify/current', function(data){
-console.log(data)
-});
+$( document ).ready(getSpotify);
+
+window.setInterval(getSpotify, 30000);
